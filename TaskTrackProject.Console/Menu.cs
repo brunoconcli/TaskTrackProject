209b1488ec6813bd704e46f7efe24e568548473a
brunoconcli <bruno.concli@becomex.com.br>
2024-11-21 16:57:16 -0300

    feat: Added GetBaseUrl static method

--- TaskTrackProject.Console/Menu.cs
+++ TaskTrackProject.Console/Menu.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Net.Http;
 using Microsoft.Extensions.Configuration;
-using TaskTrackProject.Console.Services;
 using TaskTrackProject.Console.Handlers;
+using TaskTrackProject.Console.Services;
+
 
 public static class Menu 
 {
@@ -14,5 +15,15 @@ public static class Menu
         string baseUrl = configuration["ApiSettings:BaseUrl"];
 
         MenuHandler.start();
+        // string menuHandlerTasks = await MenuHandler.GetTasks();
+        // Console.WriteLine(menuHandlerTasks);
+    }
+
+    public static string GetBaseUrl() 
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+        return configuration["ApiSettings:BaseUrl"];
     }
 }
