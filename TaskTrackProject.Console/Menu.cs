937a0718dc25a942b03ce6363adcc39657eb8554
brunoconcli <bruno.concli@becomex.com.br>
2024-11-21 15:33:28 -0300

    feat: Added Menu class (containing Main)

---
+++ TaskTrackProject.Console/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using TaskTrackProject.Console.Services;
+using TaskTrackProject.Console.Handlers;
+
+public static class Menu 
+{
+    static async Task Main(string[] args) 
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+        string baseUrl = configuration["ApiSettings:BaseUrl"];
+
+        MenuHandler.start();
+    }
+}
