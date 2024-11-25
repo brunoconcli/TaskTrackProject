c78586d319a17d93c94f64399df092a30c26732f
Bruno <brunoconcli@gmail.com>
2024-11-25 06:54:22 -0300

    wip: Implementation of remaining HTTPs, new static variable

--- TaskTrackProject.Api/Models/Task.cs
+++ TaskTrackProject.Api/Models/Task.cs
@@ -4,11 +4,11 @@ using MongoDB.Bson.Serialization.Attributes;
 
 public class Task
 {
-    [BsonId]
-    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
-    public string? Id { get; set; }
-    [BsonElement("Description")]
-    public required string Description { get; set; }
-    [BsonElement("Completed")]
-    public required bool Completed { get; set; }
+  [BsonId]
+  [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+  public string? Id { get; set; }
+  [BsonElement("Description")]
+  public required string Description { get; set; }
+  [BsonElement("Completed")]
+  public required bool Completed { get; set; }
 }
--- TaskTrackProject.Console/Handlers/MenuHandler.cs
+++ TaskTrackProject.Console/Handlers/MenuHandler.cs
@@ -9,133 +9,154 @@ using Newtonsoft.Json.Linq;
 
 public class MenuHandler
 {
-    private static HttpClient _sharedClient;
-    private static Dictionary<string, string> _menuOptions;
+  private static HttpClient _sharedClient;
+  private static Dictionary<string, string> _menuOptions;
+  private static JArray _currentTasksArray;
 
-    public static async Task start() 
+  public static async Task start()
+  {
+    _sharedClient = new HttpClient
     {
-        _sharedClient = new HttpClient
-        {
-            BaseAddress = new Uri(Menu.GetBaseUrl())
-        };
+      BaseAddress = new Uri(Menu.GetBaseUrl())
+    };
 
-        _menuOptions = new Dictionary<string, string>(){
+    _menuOptions = new Dictionary<string, string>(){
             {"1", "Exibir tarefas"},
             {"2", "Criar tarefa"},
             {"3", "Editar tarefa"},
             {"4", "Remover tarefa"},
             {"5", "Marcar como completo"}
         };
-        await DisplayMenu();
-    }
+    await DisplayMenu();
+  }
 
-    public static async Task GetTasks() 
-    {   
-        JArray jsonResponseArray = JArray.Parse(
-            (string)
-            await ApiService.GetTasksAsync(_sharedClient)
-        );
+  public static async Task GetTasks()
+  {
+    await UpdateCurrentTasksArray();
 
-        Console.WriteLine(Colors.WHITE_BOLD + "> Minhas tarefas\n" + Colors.RESET);
-        foreach (var jsonResponseObject in jsonResponseArray)
-        {
-            JObject currentObject = JObject.Parse(jsonResponseObject.ToString());
-            string checkboxPrefix = currentObject["completed"].ToObject<bool>() ? "[X]" : "[ ]";
-        
-            Console.WriteLine(checkboxPrefix + currentObject["description"]);
-        }
-        BackToMenu();
-    }
-
-    public static async Task AddTask()
+    Console.WriteLine(Colors.WHITE_BOLD + "> Minhas tarefas\n" + Colors.RESET);
+    foreach (var jsonResponseObject in _currentTasksArray)
     {
-        Console.WriteLine(Colors.WHITE_BOLD + "> Adicionar nova tarefa\nEnvie '-' para cancelar.\n" + Colors.RESET);
-        Console.Write("Descrição: ");
-        string inputDescription = Console.ReadLine();
-        
-        if (inputDescription != "-")
-        {
-            await ApiService.AddTaskAsync(_sharedClient, inputDescription, false);
-            Console.WriteLine(Colors.GREEN + "\nAtividade adicionada com sucesso." + Colors.RESET);
-        }
-        else 
-        {
-            Console.WriteLine(Colors.YELLOW + "\nAdição de nova atividade cancelada." + Colors.RESET);
-        }
-        BackToMenu();
+      JObject currentObject = JObject.Parse(jsonResponseObject.ToString());
+      string checkboxPrefix = currentObject["completed"].ToObject<bool>() ? "[X] " : "[ ] ";
+
+      Console.WriteLine(checkboxPrefix + currentObject["description"]);
     }
+  }
 
-    // public  void UpdateTask() 
-    // {
+  public static async Task AddTask()
+  {
+    Console.WriteLine(Colors.WHITE_BOLD + "> Adicionar nova tarefa\nEnvie '-' para cancelar.\n" + Colors.RESET);
+    Console.Write("Descrição: ");
+    string inputDescription = Console.ReadLine();
 
-    // }
-
-    // public  void RemoveTask()
-    // {
-
-    // }
-
-    // public  void MarkAsComplete() 
-    // {
-
-    // }
-    static async Task DisplayMenu(string infoMessage = "") 
+    if (inputDescription != "-")
     {
-        Console.Clear();
-        Console.WriteLine("Hoje:{} | Última atividade: ");
-        
-        foreach(KeyValuePair<string, string> option in _menuOptions)
-        {
-            Console.WriteLine("{0}. {1}", option.Key, option.Value);
-        }
-
-        Console.Write("\n" + infoMessage + "\nEscolha o que deseja fazer (1-5): ");
-        string chosenOption = Console.ReadLine();
-        await HandleOptionChoice(chosenOption);
+      await ApiService.AddTaskAsync(_sharedClient, inputDescription, false);
+      Console.WriteLine(Colors.GREEN + "\nAtividade adicionada com sucesso." + Colors.RESET);
     }
-
-    static async Task HandleOptionChoice (string chosenOption)
+    else
     {
-        string infoMessage = "";
-        if (!int.TryParse(chosenOption, out _)) {
-            infoMessage = Colors.RED + "A opção escolhida deve ser um número" + Colors.RESET;
-        }
-        else if (int.Parse(chosenOption) > 5 || int.Parse(chosenOption) < 0)
-        {
-            infoMessage = Colors.RED + "A opção deve ser um número positivo menor ou igual a 5" + Colors.RESET;
-        }
-        else
-        {
-            Console.Clear();
-            switch(chosenOption) 
-            {
-                case "1": 
-                    await GetTasks();
-                    break;
-                case "2":
-                    await AddTask();
-                    break;
-            }
-        }
-        await DisplayMenu(infoMessage);
+      Console.WriteLine(Colors.YELLOW + "\nAdição de nova atividade cancelada." + Colors.RESET);
+    }
+  }
+
+  // public  void UpdateTask() 
+  // {
+
+  // }
+
+  public static async Task DeleteTask()
+  {
+    await UpdateCurrentTasksArray();
+    Console.WriteLine(Colors.WHITE_BOLD + "> Remover tarefa\nPressione [Enter] para cancelar.\n" + Colors.RESET);
+
+    let counter = 0; // stawped heah [15:44]
+    foreach (var jsonResponseObject in _currentTasksArray)
+    {
+      JObject currentObject = JObject.Parse(jsonResponseObject.ToString());
+      string checkboxPrefix = currentObject["completed"].ToObject<bool>() ? "[X] " : "[ ] ";
+
+      Console.WriteLine(checkboxPrefix + currentObject["description"]);
+    }
+    string response = await ApiService.DeleteTaskAsync();
+    Console.WriteLine(response);
+  }
+
+  // public  void MarkAsComplete() 
+  // {
+
+  // }
+  static async Task DisplayMenu(string infoMessage = "")
+  {
+    Console.Clear();
+    Console.WriteLine("Hoje:{} | Última atividade: ");
+
+    foreach (KeyValuePair<string, string> option in _menuOptions)
+    {
+      Console.WriteLine("{0}. {1}", option.Key, option.Value);
     }
 
-    static void BackToMenu()
-    {
-        Console.WriteLine("\nRetornar para o menu...");
-        Console.ReadLine();
-    }
+    Console.Write("\n" + infoMessage + "\nEscolha o que deseja fazer (1-5): ");
+    string chosenOption = Console.ReadLine();
+    await HandleOptionChoice(chosenOption);
+  }
 
-    static void Exit() 
+  static async Task HandleOptionChoice(string chosenOption)
+  {
+    string infoMessage = "";
+    if (!int.TryParse(chosenOption, out _))
     {
-        // RegisterActivity();
-        Console.WriteLine("Encerrando aplicação...");
-        Environment.Exit(0);
+      infoMessage = Colors.RED + "A opção escolhida deve ser um número" + Colors.RESET;
     }
+    else if (int.Parse(chosenOption) > 5 || int.Parse(chosenOption) < 0)
+    {
+      infoMessage = Colors.RED + "A opção deve ser um número positivo menor ou igual a 5" + Colors.RESET;
+    }
+    else
+    {
+      Console.Clear();
+      switch (chosenOption)
+      {
+        case "1":
+          await GetTasks();
+          break;
+        case "2":
+          await AddTask();
+          break;
+        case "4":
+          await DeleteTask();
+          break;
+      }
+      BackToMenu();
+    }
+    await DisplayMenu(infoMessage);
+  }
 
-    static string GetUsersOS() 
-    {
-        // TO-DO
-        return "";
-    }
+  static async Task UpdateCurrentTasksArray()
+  {
+    _currentTasksArray = JArray.Parse(
+        (string)
+        await ApiService.GetTasksAsync(_sharedClient)
+    );
+  }
+
+  static void BackToMenu()
+  {
+    Console.WriteLine("\nRetornar para o menu...");
+    Console.ReadLine();
+  }
+
+  static void Exit()
+  {
+    // RegisterActivity();
+    Console.WriteLine("Encerrando aplicação...");
+    Environment.Exit(0);
+  }
+
+  static string GetUsersOS()
+  {
+    // TO-DO
+    return "";
+  }
 }
